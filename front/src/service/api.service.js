c18d501eb980854168b242bb8d3876bd7dc273f3
vickcm <77595917+vickcm@users.noreply.github.com>
2023-06-13 21:31:20 -0300

    Update api.service.js

--- front/src/service/api.service.js
+++ front/src/service/api.service.js
@@ -1,25 +1,29 @@
 export async function call({ uri, method = 'GET', body = undefined }) {
     return fetch(`http://localhost:2023/api/${uri}`, {
-        headers: {
-            'auth-token': localStorage.getItem('token'),
-            'Content-Type': 'application/json'
-        },
-        method,
-        body: JSON.stringify(body)
+      headers: {
+        'auth-token': localStorage.getItem('token'),
+        'Content-Type': 'application/json'
+      },
+      method,
+      body: JSON.stringify(body)
     })
-        .then(async response => {
-            if (!response.ok) {
-
-                if (response.status === 401) {
-                    localStorage.removeItem('token')
-                }
-
-                throw await response.json()
-            }
-            return response.json()
-        })
-}
-
-export default {
+      .then(async response => {
+        console.log(response); // Agregado para depuración
+  
+        if (!response.ok) {
+          if (response.status === 401) {
+            localStorage.removeItem('token');
+          }
+          const errorData = await response.json(); // Obtener el cuerpo de la respuesta como JSON
+          throw errorData; // Lanzar el objeto errorData
+        }
+  
+        const responseData = await response.json(); // Obtener el cuerpo de la respuesta como JSON
+        return responseData; // Devolver el objeto responseData
+      });
+  }
+  
+  export default {
     call
-}
+  }
+  
