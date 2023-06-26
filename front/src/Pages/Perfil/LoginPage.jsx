3c8b1ae64f92571ae0b3d51fdd5dc39b4b8822f3
Chechu Barco <86536881+chebarco@users.noreply.github.com>
2023-06-25 21:34:18 -0300

    ojito password

--- front/src/Pages/Perfil/LoginPage.jsx
+++ front/src/Pages/Perfil/LoginPage.jsx
@@ -1,70 +1,92 @@
-import { useState} from 'react'
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import '../../css/LoginPage.css'
-import { useNavigate } from 'react-router-dom';
-import authService from '../../service/autentication.service.js';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "../../css/LoginPage.css";
+import { useNavigate } from "react-router-dom";
+import authService from "../../service/autentication.service.js";
+import { Link } from "react-router-dom";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
+
 
 // para hacer ** cuando el usuario ya tiene perfil creado, llevarlo a la pagina de desafios, si no lo tiene llevarlo a crear perfil
 
-function LoginPage(){
-    const navigate = useNavigate()
+function LoginPage() {
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [shown, setShown] = useState(false);
+  const switchShown = () => setShown(!shown);
 
-    const onChangeEmail = (event) =>{
-        setEmail(event.target.value)
-    }
+  
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
 
-    const onChangePassword = (event) =>{
-        setPassword(event.target.value)
-    }
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
-    const onSubmit = (event) => {
-        event.preventDefault() 
-        console.log('submit', email, password)
-        authService.login({email, password})
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log("submit", email, password);
+    authService
+      .login({ email, password })
 
-        .then((data) => {
-            console.log("data", data.responseAccount.token)
-            setError('')
-            localStorage.setItem('token', data.responseAccount.token)
-            navigate('/desafio', {replace: true})
-        })
-        .catch(err => {
-            setError(err.error.message)
-        })
-    } 
+      .then((data) => {
+        console.log("data", data.responseAccount.token);
+        setError("");
+        localStorage.setItem("token", data.responseAccount.token);
+        navigate("/desafio", { replace: true });
+      })
+      .catch((err) => {
+        setError(err.error.message);
+      });
+  };
 
-    return(
-        <div className="page">
-            <div className="container">
-                <Form onSubmit={onSubmit} className='form-login'> 
-                    <h1 className="text-center">Iniciar Sesión</h1>
-                    <Form.Group className="col-mb-6">
-                        <Form.Label>Email:</Form.Label>
-                        <Form.Control type="email"  placeholder="Tu email" value={email} onChange={onChangeEmail} />
-                    </Form.Group>
-                    <Form.Group className="col-mb-6" >
-                        <Form.Label>Contraseña</Form.Label>
-                        <Form.Control type="password" placeholder="Tu clave" value={password} onChange={onChangePassword}/>
-                    </Form.Group>
-                    <p> {error} </p>
-                    <div className='row justify-content-center my-4'>
-                        <Button type="submit" className="button">
-                            Ingresar
-                        </Button>
-                        <Link to="/register" className="btn mt-2">
-                            ¿Aún no tienes cuenta? ¡Regístrate!
-                        </Link>
-                    </div>
-                </Form> 
+  return (
+    <div className="page">
+      <div className="container">
+        <Form onSubmit={onSubmit} className="form-login">
+          <h1 className="text-center">Iniciar Sesión</h1>
+          <Form.Group className="col-mb-6">
+            <Form.Label>Email:</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Tu email"
+              value={email}
+              onChange={onChangeEmail}
+            />
+            
+          </Form.Group>
+          <Form.Group className="col-mb-6">
+            <Form.Label>Contraseña</Form.Label>
+            <div className="input-with-icon">
+              <Form.Control
+                type={shown ? "text" : "password"}
+                placeholder="Tu clave"
+                value={password}
+                onChange={onChangePassword}
+              />
+              <button onClick={switchShown} className="eye-icon-button">
+                {shown ? <AiFillEyeInvisible /> : <AiFillEye />}
+              </button>
             </div>
-        </div>
-    )
+          </Form.Group>
+          <p> {error} </p>
+          <div className="row justify-content-center my-4">
+            <Button type="submit" className="button">
+              Ingresar
+            </Button>
+            <Link to="/register" className="btn mt-2">
+              ¿Aún no tienes cuenta? ¡Regístrate!
+            </Link>
+          </div>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
-export default LoginPage
+export default LoginPage;
--- front/src/Pages/Perfil/RegisterPage.jsx
+++ front/src/Pages/Perfil/RegisterPage.jsx
@@ -1,66 +1,87 @@
-
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import '../../css/LoginPage.css'
-import authService from '../../service/autentication.service.js';
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import "../../css/LoginPage.css";
+import authService from "../../service/autentication.service.js";
 import { useCallback, useState } from "react";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 
-function RegisterPage(){
-    const navigate = useNavigate()
+function RegisterPage() {
+  const navigate = useNavigate();
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [error, setError] = useState('')
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const [shown, setShown] = useState(false);
+  const switchShown = () => setShown(!shown);
 
-    const onChangeEmail = (event) =>{
-        setEmail(event.target.value)
-    }
+  const onChangeEmail = (event) => {
+    setEmail(event.target.value);
+  };
 
-    const onChangePassword = (event) =>{
-        setPassword(event.target.value)
-    }
+  const onChangePassword = (event) => {
+    setPassword(event.target.value);
+  };
 
-    const onSubmit = useCallback((event) => {
-        event.preventDefault()
-        console.log("submit", email, password)
-        const account = {
-            email,
-            password
-        }
-        authService.createAccount(account)
-    
-        .then(({account}) => {
-            console.log("cuenta creada", account)
-            setError('')
-            navigate('/login', {replace: true})
+  const onSubmit = useCallback(
+    (event) => {
+      event.preventDefault();
+      console.log("submit", email, password);
+      const account = {
+        email,
+        password,
+      };
+      authService
+        .createAccount(account)
+
+        .then(({ account }) => {
+          console.log("cuenta creada", account);
+          setError("");
+          navigate("/login", { replace: true });
         })
-        .catch(err => {
-            setError(err.error.message)
-        })
-    }, [email, password, navigate, setError])
+        .catch((err) => {
+          setError(err.error.message);
+        });
+    },
+    [email, password, navigate, setError]
+  );
 
-    return(
-        <div className="page">
-            <div className="container">
-                <Form className="form-login" onSubmit={onSubmit}> 
-                    <h1 className="text-center">Registrarse</h1>
-                    <Form.Group className="col-mb-6">
-                        <Form.Label>Email:</Form.Label>
-                        <Form.Control type="email"  placeholder="Tu email" value={email} onChange={onChangeEmail} />
-                    </Form.Group>
-                    <Form.Group className="col-mb-6" >
-                        <Form.Label>Contraseña</Form.Label>
-                        <Form.Control type="password" placeholder="Tu clave" value={password} onChange={onChangePassword}/>
-                    </Form.Group>
-                    <p> {error} </p>
-                    <Button type="submit" className="button">
-                        Registrarme
-                    </Button>
-                </Form> 
-            </div>
-        </div>
-    )
+  return (
+    <div className="page">
+      <div className="container">
+        <Form className="form-login" onSubmit={onSubmit}>
+          <h1 className="text-center">Registrarse</h1>
+          <Form.Group className="col-mb-6">
+            <Form.Label>Email:</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Tu email"
+              value={email}
+              onChange={onChangeEmail}
+            />
+          </Form.Group>
+          <Form.Group className="col-mb-6">
+            <Form.Label>Contraseña</Form.Label>
+            <div className="input-with-icon">
+              <Form.Control
+                type={shown ? "text" : "password"}
+                placeholder="Tu clave"
+                value={password}
+                onChange={onChangePassword}
+              />
+              <button type="button" onClick={switchShown} className="eye-icon-button">
+                {shown ? <AiFillEyeInvisible /> : <AiFillEye />}
+              </button>
+            </div>{" "}
+          </Form.Group>
+          <p> {error} </p>
+          <Button type="submit" className="button">
+            Registrarme
+          </Button>
+        </Form>
+      </div>
+    </div>
+  );
 }
 
-export default RegisterPage
+export default RegisterPage;
