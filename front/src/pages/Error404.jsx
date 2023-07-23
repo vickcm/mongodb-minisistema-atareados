0c75ad40e32a5de8e8ad3ab30bc7137483c32bb0
vickcm <77595917+vickcm@users.noreply.github.com>
2023-07-23 14:37:36 -0300

    404

--- front/src/pages/Error404.jsx
+++ front/src/pages/Error404.jsx
@@ -1,12 +1,23 @@
-import '../css/Error404Estilos.css'
+import React from "react";
+import "../css/Error404Estilos.css";
+import { Link } from "react-router-dom";
+import ImageHome from "../imagenes/Home.png";
+import Image from "react-bootstrap/Image";
 
 function Error404() {
   return (
     <div className="Error404">
+      <div className="img-home">
+        <Image src={ImageHome} className="image" />
+      </div>
       <h1>404</h1>
       <p>Página no encontrada</p>
+
+      <Link to="/" className="btn-home">
+        Volver al inicio
+      </Link>
     </div>
-  )
+  );
 }
 
-export default Error404
+export default Error404;
