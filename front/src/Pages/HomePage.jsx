00bc036d9b50f7ed5389c363c69a64969557d438
vickcm <77595917+vickcm@users.noreply.github.com>
2023-07-24 20:32:11 -0300

    Update HomePage.jsx

--- front/src/Pages/HomePage.jsx
+++ front/src/Pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import "../css/mainCss.css";
 import Col from "react-bootstrap/Col";
 import Container from "react-bootstrap/Container";
-import Image from "react-bootstrap/Image";
+import { motion } from "framer-motion"; // Importar motion desde Framer Motion
 import Row from "react-bootstrap/Row";
 import ImageHome from "../imagenes/Home.png";
 import { Link } from "react-router-dom";
@@ -15,23 +15,52 @@ function Homepages() {
       <Container>
         <Row>
           <Col>
-            <div className="img-home">
-              <Image src={ImageHome} className="image" />
-            </div>
+            {/* Utilizar motion.img en lugar de Image */}
+            <motion.div
+              initial={{ opacity: 0, scale: 0.5 }} // Propiedades iniciales de la animación
+              animate={{ opacity: 1, scale: 1 }} // Propiedades animadas
+              transition={{ duration: 0.5 }} // Duración de la animación
+              className="img-home"
+            >
+              <motion.img
+                src={ImageHome}
+                alt="Home"
+                className="image"
+              />
+            </motion.div>
           </Col>
-          <div className="aling-button">
-            <button className="btn-home">
+          <motion.div
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.2, duration: 0.5 }}
+            className="aling-button"
+          >
+            {/* Utilizar motion.button en lugar de button */}
+            <motion.button
+              whileHover={{ scale: 1.1 }} // Animación al pasar el mouse sobre el botón
+              className="btn-home"
+            >
               {" "}
               <Link to="/paneldesafio" className="btn">
                 Ver Panel
               </Link>
-            </button>
-          </div>
-          <div className="aling-a">
-            <Link to="/desafio" className="btn">
-              ¿Quieres crear un desafio?{" "}
-            </Link>
-          </div>
+            </motion.button>
+          </motion.div>
+          <motion.div
+            initial={{ y: 50, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ delay: 0.3, duration: 0.5 }}
+            className="aling-a"
+          >
+            {/* Utilizar motion.a en lugar de Link */}
+            <motion.a
+              whileHover={{ scale: 1.1 }} // Animación al pasar el mouse sobre el enlace
+              href="/desafio"
+              className="btn"
+            >
+              ¿Quieres crear un desafío? Anímate a jugar
+            </motion.a>
+          </motion.div>
         </Row>
       </Container>
     </>
